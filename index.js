b5622f02bc5332b76fae6edcab05ea666efd23eb
Vince Ivan Minez <banongofficial28@gmail.com>
2023-08-18 20:30:18 +0800

    Login page and Home page created using EJS

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
+app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended : true}))
+
+
 app.get("/", function (req, res) {
-    res.send("TEST CONNECTION");
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+
+app.post("/login", function (req, res) {
+    res.send("POST TEST")
 })
 
 app.listen("3000", function () {
