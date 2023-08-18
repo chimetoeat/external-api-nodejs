50b647a044c5194ec91785a5bcdf5494a8611e19
Vince Ivan Minez <banongofficial28@gmail.com>
2023-08-18 18:58:36 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("TEST CONNECTION");
+})
+
+app.listen("3000", function () {
+    console.log("Server started at port 3000.");
+  });
