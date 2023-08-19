b2824d31122cd691ebf4f3f5d1f8c235efbaf9af
Vince Ivan Minez <banongofficial28@gmail.com>
2023-08-19 21:42:56 +0800

    Revert back

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.static(__dirname + '/public'))
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended : true}));
 app.use(express.json());
